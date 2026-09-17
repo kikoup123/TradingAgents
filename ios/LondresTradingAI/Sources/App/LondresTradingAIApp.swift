e257384a81021f9f1b8762cc8531e3b8923b4595
kikoup123 <kfd7tmmh7m@privaterelay.appleid.com>
2026-09-17 04:47:08 +0300

    Wire cTrader broker store into iOS app

--- ios/LondresTradingAI/Sources/App/LondresTradingAIApp.swift
+++ ios/LondresTradingAI/Sources/App/LondresTradingAIApp.swift
@@ -6,6 +6,7 @@ struct LondresTradingAIApp: App {
     @StateObject private var journalStore = JournalStore()
     @StateObject private var paperTradingStore = PaperTradingStore()
     @StateObject private var subscriptionStore = SubscriptionStore()
+    @StateObject private var cTraderBrokerStore = CTraderBrokerStore()
 
     var body: some Scene {
         WindowGroup {
@@ -14,8 +15,15 @@ struct LondresTradingAIApp: App {
                 .environmentObject(journalStore)
                 .environmentObject(paperTradingStore)
                 .environmentObject(subscriptionStore)
+                .environmentObject(cTraderBrokerStore)
+                .onOpenURL { url in
+                    Task {
+                        await cTraderBrokerStore.handleCallback(url)
+                    }
+                }
                 .task {
                     await subscriptionStore.load()
+                    await cTraderBrokerStore.refreshStatus()
                     await MainActor.run {
                         appModel.startMarketData(
                             paperTradingStore: paperTradingStore,
